a7d00beaeab92298b77ce51d2c495b160f569ad7
Greg Munn <greg@sgmunn.com>
2015-08-11 15:19:32 -0400

    Initial commit

---
+++ Bug32417/Bug32417/AwesomeClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Bug32417
+{
+	public class AwesomeClass
+	{
+		public AwesomeClass()
+		{
+		}
+	}
+
+	public class SuperAwesomeClass
+	{
+		public string Super { get; set; }
+	}
+}
+
---
+++ Bug32417/Bug32417/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Bug32417
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			var x = new AwesomeClass ();
+			var y = new SuperAwesomeClass ();
+			y.Super = "Mr Incredible";
+
+			Console.WriteLine(y.Super);
+		}
+	}
+}
